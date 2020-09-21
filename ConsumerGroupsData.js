f1d2e08386972a3aed1066278ee2cfc97c2424d5
suyash-naithani <suyash.naithani@gmail.com>
2020-09-21 13:30:43 +0530

    Fix indentation

--- ConsumerGroupsData.js
+++ ConsumerGroupsData.js
@@ -1,25 +1,25 @@
 const kafka = require('kafka-node');
 const client = new kafka.KafkaClient();
-const admin = new kafka.Admin(client); 
-var consumerGroupsList =[]
+const admin = new kafka.Admin(client);
+var consumerGroupsList = []
 var consumerGroupsData = {};
-let count =0;
+let count = 0;
 
-module.exports.consumerGroupsInfo =  function(){
+module.exports.consumerGroupsInfo = function () {
 
 
   admin.listGroups((err, res) => {
-  consumerGroupsList=Object.keys(res)
-consumerGroupsList.forEach(consumerGroup => {
-admin.describeGroups([consumerGroup], (err, res) => {
-   consumerGroupsData[count] = res;
-   count+=1;
-})
-})
+    consumerGroupsList = Object.keys(res)
+    consumerGroupsList.forEach(consumerGroup => {
+      admin.describeGroups([consumerGroup], (err, res) => {
+        consumerGroupsData[count] = res;
+        count += 1;
+      })
+    })
 
-})
+  })
 
-return consumerGroupsData
+  return consumerGroupsData
 }
 
 
