43014fa3a8d9fa772550d513936b365f430972d5
suyash-naithani <suyash.naithani@gmail.com>
2020-09-21 12:19:56 +0530

    Remove commented code

--- ConsumerGroupsData.js
+++ ConsumerGroupsData.js
@@ -1,6 +1,6 @@
 const kafka = require('kafka-node');
 const client = new kafka.KafkaClient();
-const admin = new kafka.Admin(client); // client must be KafkaClient
+const admin = new kafka.Admin(client); 
 const express = require('express')
 const app = express()
 const port = 3000
@@ -15,28 +15,16 @@ module.exports.consumerGroupsInfo =  function(){
   consumerGroupsList=Object.keys(res)
 consumerGroupsList.forEach(consumerGroup => {
 admin.describeGroups([consumerGroup], (err, res) => {
-  //console.log(JSON.stringify(res,null,1));
-  //consumerGroupsList=res;
    consumerGroupsData[count] = res;
-   //console.log(consumerGroupsData);
    count+=1;
 })
 })
 
 })
 
-//console.log(consumerGroupsData)
 return consumerGroupsData
 }
 
-/*result()
-app.get('/', (req, res) => {
-  res.send(result())
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})*/
 
 
 
